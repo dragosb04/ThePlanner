6f882f4a2ee87d287c0b5df71e6e96beb6f89a62
dragosb04 <dragosinfinit10@gmail.com>
2025-07-23 01:03:20 +0300

    Fix registration error

--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -15,7 +15,7 @@ function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:3001/register', {
+    const response = await fetch('http://localhost:3000/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password, username }),
